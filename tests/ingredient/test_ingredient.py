9c2adeb764e23259dbd4540c932211844e63e218
Ana Lourenco <analuizajl5@gmail.com>
2024-03-21 15:41:35 -0300

    comecando

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,28 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    first_ingredient = Ingredient("camarão")
+    second_ingredient = Ingredient("manteiga")
+    third_ingredient = Ingredient("camarão")
+
+    assert third_ingredient.name == "camarão"
+    assert first_ingredient.name == third_ingredient.name
+    assert second_ingredient.name != third_ingredient.name
+
+    assert (first_ingredient == third_ingredient) is True
+    assert (first_ingredient == second_ingredient) is False
+
+    assert hash(first_ingredient) == hash(third_ingredient)
+    assert hash(first_ingredient) != hash(second_ingredient)
+
+    assert repr(first_ingredient) == "Ingredient('camarão')"
+    assert (first_ingredient.restrictions) == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
